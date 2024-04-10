37da2c7a489050c3d3546fe6c4fb2f5b2893ae0f
BOW-ON <qhdnjs94@gmail.com>
2024-04-10 13:18:10 +0900

    0410 서보원 딜리트 폼버튼 작업

--- src/delete_otter_all_delete.php
+++ src/delete_otter_all_delete.php
@@ -1,4 +1,8 @@
 <?php
+// 전체 삭제 PHP
+
+
+
 // 설정 정보
 require_once( $_SERVER["DOCUMENT_ROOT"]."/config_sbw.php"); // 설정 파일 호출																		
 require_once(FILE_LIB_DB); // DB관련 라이브러리
@@ -8,29 +12,18 @@ try {
     $conn = my_db_conn(); // connection 함수 호출
 
     if (REQUEST_METHOD === "POST") {
-        // 파라미터 획득
-        $no = isset($_POST["board_no"]) ? $_POST["board_no"] : "";
-        
-        $arr_err_param = [];
-        if($board_no === ""){
-           $arr_err_param[] = "board_no";
-        }
-        if(count($arr_err_param) > 0) {
-            throw new Exception("Parameter Error : ".implode(",", $arr_err_param));
-        }
-  
+
         //Transaction 시작
         $conn->beginTransaction();
   
-        // 게시글 정보 삭제
-        $arr_param = [
-            "board_no" => $board_no
-        ];
-        $result = db_restore_boards($conn, $arr_param);
+        // 게시글 정보 전체 삭제
+
+        $result = db_boards_all_delete($conn);
+        
   
-        // 삭제 예외 처리
-        if($result !== 1 ){
-          throw new Exception("Delete Boards no count");
+        // 복구 예외 처리
+        if($resul === 0 ){
+          throw new Exception("삭제할 파일이 없습니다.");
         }
   
         // commmit

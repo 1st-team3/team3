c69dac47d63b1228006a573dc6e80b0d3aa5126f
Kwonseonghwan <ksh5145@gmail.com>
2024-04-11 23:27:06 +0900

    0411 권성환 디테일수정

--- src/memo_list_ksh.php
+++ src/memo_list_ksh.php
@@ -5,12 +5,15 @@ require_once(FILE_LIB_DB);
 try {
     $conn = my_db_conn();
 
-
-
-    $result_board = db_select_boards_all($conn);
-    $result_memo = db_select_memos_all($conn);
     
 
+
+
+
+
+
+
+    $result_memo = db_select_memos_all($conn);
 } catch (\Throwable $e) {
     echo $e->getMessage();
     exit;
@@ -25,27 +28,17 @@ try {
     <h2>MEMO</h2>
     <div class="memo-board">
         <div class="memo-textarea">
-            <form action="./memo_delete_sbw.php" method="post">
             <?php foreach ($result_memo as $item){ ?>
-                <div class="memo_main">
-                    <div class="memo_con"><?php echo $item["memo_content"] ?></div>
-                    <input type="hidden" name="memo_no" value="<?php echo $item["memo_no"]; ?>">
-                    <button type="submit" class ="divbutton">x</button>
-                </div>
+                <div class="memo_main"><div class="memo_con"><?php echo $item["memo_content"] ?></div>
+                <form action="memo_delete_ksh.php" method="post">
+                <input type="hidden" name="memo_no" value="<?php echo $item["memo_no"]; ?>">
+                <button type="submit" class ="divbutton">x</button></form></div>
             <?php } ?>
-            <?php foreach ($result_board as $item){ ?>
-                <div class="memo_main">
-                    <input type="hidden" name="memo_no" value="<?php echo $item["board_no"]; ?>">
-                </div>
-            <?php } ?>
-
-            </form>
         </div>
 
         <div class="text-button">
             <form action="./memo_insert_ksh.php" method="post" >
                 <input type="text" class="memo-text" autocomplete="off" name="memo_content">
-                                    <input type="hidden" name="memo_no" value="<?php echo $item["board_no"]; ?>">
                 <button class="sudal-button" type="submit" ><img class="sudal-head" src="../image/otter_face_end.png"></button>
             </form>
         </div>

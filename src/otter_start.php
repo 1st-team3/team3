2b3ce0e161466d627cbfaf4003ea383f750679fd
BOW-ON <qhdnjs94@gmail.com>
2024-04-12 11:08:23 +0900

    0412 서보원 스타트 작업

---
+++ src/otter_start.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/otter_start.css">
+</head>
+<body>
+    <div class="main">
+    <h1>TODOLIST</h1>
+    <div class="float">
+<input type="text" id="here"  placeholder="Your name here">
+    <a href="./otter_main.php">Press</a>
+</div>
+</div>
+</body>
+</html>

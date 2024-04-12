f5149ba0a0d63825d59392c8bdf8dd9af067a275
BOW-ON <qhdnjs94@gmail.com>
2024-04-12 15:34:31 +0900

    0412 서보원 config 파일 수정

--- src/delete_otter_delete.php
+++ src/delete_otter_delete.php
@@ -4,7 +4,7 @@
 
 
 // 설정 정보
-require_once( $_SERVER["DOCUMENT_ROOT"]."/config_sbw.php"); // 설정 파일 호출																		
+require_once( $_SERVER["DOCUMENT_ROOT"]."/config.php"); // 설정 파일 호출																		
 require_once(FILE_LIB_DB); // DB관련 라이브러리
 
 try {

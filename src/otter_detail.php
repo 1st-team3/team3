abf385a01dccfdbf4e223f5b12ec88d8a75efdde
breadhouse1 <holiclong@naver.com>
2024-04-12 15:05:57 +0900

    0412 권현수 추가수정

--- src/otter_detail.php
+++ src/otter_detail.php
@@ -90,6 +90,13 @@
             }
 
             $item = $result[0];
+            $created_at = $item["created_at"]; // 예시 데이터베이스에서 날짜 정보를 가져온다고 가정
+            // 가져온 날짜 정보를 이용하여 연도와 월을 추출합니다.
+            $day = date('j', strtotime($created_at));
+            $mon = date('n', strtotime($created_at));
+            // BACK_BTN의 링크에 GET 파라미터를 추가합니다.
+            
+            $back_btn_link = "./otter_list.php?year=$year&month=$mon&date=$day";
 
 
 
@@ -156,7 +163,17 @@
                     </div>
                 </div>
                 <div class="folder_back">
-                    <div class="folder_back_btn"><a href="<?php echo $previous_page; ?>" class="back_btn">◁</a></div>
+
+                    <?php $created_at = $item["created_at"]; // 예시 데이터베이스에서 날짜 정보를 가져온다고 가정
+                    // 가져온 날짜 정보를 이용하여 연도와 월을 추출합니다.
+                    $day = date('j', strtotime($created_at));
+                    $mon = date('n', strtotime($created_at));
+                    // BACK_BTN의 링크에 GET 파라미터를 추가합니다.
+                    $back_btn_link = "./otter_list.php?year=$year&month=$mon&date=$day";
+                    ?>
+
+                    <div class="folder_back_btn"><a href="<?php echo $back_btn_link; ?>" class="back_btn">◁</a></div>
+
                     <div class="folder_back_square"></div>
                 </div>
             </div>
